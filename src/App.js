05646e79d9a0e4c24f55d7593a25f3861bdef435
Gloria Pucoe <79689244+Gloria07-art@users.noreply.github.com>
2023-10-18 16:52:27 +0200

    Dream

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import { Home } from './Componenets/Home';
+import { Routes, Route } from 'react-router-dom';
+import {SignIn} from "./Componenets/SignIn"
+import { SignUp } from './Componenets/SignUp';
+import {ForgotPassword} from './Componenets/ForgotPassword'
 import './App.css';
 
 function App() {
+
+  console.log()
+  
   return (
-    <div >
-      <Home/>
+    <div className="App">
+      <>
+      <Routes>
+        <Route path='/' element={<SignIn/>}/>
+        <Route path='SignUp' element={<SignUp/>}/>
+        <Route path='ForgotPassword' element={<ForgotPassword/>}/>
+        <Route path='Home' element={<Home/>}/>
+        </Routes>
+        </>
+     
     </div>
   );
 }
